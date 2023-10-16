cd18f6a1bb658b02e324c730750f61aae8b2396a
VorobeiIvan <0675361905vi@gmail.com>
2023-10-16 19:34:11 +0300

    add task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,81 +1,29 @@
-// import { galleryItems } from './gallery-items.js';
-
-// const ul = document.querySelector('.gallery');
-
-// galleryItems.forEach(function ({ preview, original, description }) {
-//   const li = document.createElement('li');
-//   li.classList.add('gallery__item');
-//   const a = document.createElement('a');
-//   a.classList.add('gallery__link');
-//   a.href = original;
-//   const img = document.createElement('img');
-//   img.classList.add('gallery__image');
-//   img.src = preview;
-//   img.dataSource = original;
-//   img.alt = description;
-
-//   ul.appendChild(li);
-//   li.appendChild(a);
-//   a.appendChild(img);
-// });
-
-// ul.addEventListener('click', openOriginalIMG);
-
-// function openOriginalIMG(e) {
-//   e.preventDefault();
-//   const imgOriginal = e.target.dataSource;
-//   const instance = basicLightbox.create(`
-//     <img src="${imgOriginal}" width="800" height="600">
-// `);
-
-//   instance.show();
-
-//   function closeModal() {
-//     instance.close();
-//   }
-
-//   document.addEventListener('keyup', e => {
-// if (e.key === 'Escape') {
-//   closeModal();
-// }
-// if (e.key === 'Enter') {
-//   closeModal();
-// }
-//   });
-// }
-
 import { galleryItems } from './gallery-items.js';
 
-const gallery = document.querySelector('.gallery');
-const galleryImages = document.querySelectorAll('.gallery__image');
+const ul = document.querySelector('.gallery');
 
-function createMarkup(galleryItems) {
-  return galleryItems
-    .map(
-      ({ preview, original, description }) => `
-      <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </li>`
-    )
-    .join('');
-}
+galleryItems.forEach(function ({ preview, original, description }) {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+  const a = document.createElement('a');
+  a.classList.add('gallery__link');
+  a.href = original;
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = preview;
+  img.dataSource = original;
+  img.alt = description;
 
-gallery.innerHTML = createMarkup(galleryItems);
+  ul.appendChild(li);
+  li.appendChild(a);
+  a.appendChild(img);
+});
 
-gallery.addEventListener('click', openOriginalIMG);
+ul.addEventListener('click', openOriginalIMG);
 
 function openOriginalIMG(e) {
   e.preventDefault();
-
-  const imgOriginal = e.target.dataset.source;
-
+  const imgOriginal = e.target.dataSource;
   const instance = basicLightbox.create(`
     <img src="${imgOriginal}" width="800" height="600">
 `);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryImages = document.querySelectorAll('.gallery__image');
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+gallery.innerHTML = createMarkup(galleryItems);
+
+gallery.addEventListener('click', openOriginalIMG);
+
+function openOriginalIMG(e) {
+  e.preventDefault();
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+}

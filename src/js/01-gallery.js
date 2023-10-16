fffbbd6bdbf64b3bec81dfbc94e86acf40eb3e9c
VorobeiIvan <0675361905vi@gmail.com>
2023-10-16 14:55:07 +0300

    add start files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,9 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`);
+
+instance.show();
 
 console.log(galleryItems);
